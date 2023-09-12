ec5185c616df6fcfc88ebeff445b1c9540e52193
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-12 12:27:49 +0530

    MODIFIED: rendering helloworld component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './components/hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HelloWorldComponent]
 })
 export class AppModule { }
