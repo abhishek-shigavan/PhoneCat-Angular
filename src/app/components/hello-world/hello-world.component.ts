4628a56aface450fbf32cbb311ff007d3d9b8742
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2023-09-12 14:42:22 +0530

    MODIFIED: added a method to show content & to toggle visibility of content

--- src/app/components/hello-world/hello-world.component.ts
+++ src/app/components/hello-world/hello-world.component.ts
@@ -7,11 +7,31 @@ import { Component } from '@angular/core';
 })
 export class HelloWorldComponent {
   message = "passing message from component"
-
+  visibility = true
   handleClick() {
     console.log("Clicked")
   }
 
   spanId = "demoSpan"
   spanColor = "#687869"
+  name = ""
+  display = "block"
+
+  handleToggleVisibility() {
+    if(this.visibility) { 
+      this.visibility = false
+      this.display = "none"
+    } else {
+      this.visibility = true
+      this.display = "block"
+    }
+  }
+
+  handlePrintName() {
+    this.name = "xyz"
+    if(!this.visibility) {
+      this.visibility = true
+      this.display = "block"
+    }
+  }
 }
